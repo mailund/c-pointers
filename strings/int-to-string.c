a0ea89335343be106af6879d92d319fd1ef3eb56
Thomas Mailund <mailund@birc.au.dk>
2020-09-11 07:58:04 +0200

    index and offset int to string

--- strings/int-to-string.c
+++ strings/int-to-string.c
@@ -6,17 +6,17 @@
 int no_digits(int n)
 {
   assert(n > 0);
-  return log10(n) + 1;
+  return (int)log10(n) + 1;
 }
 
 void pos_int_to_string(int n, char *s)
 {
   assert(n > 0);
   static const char *digits = "0123456789";
-  s += no_digits(n) + 1;
-  *(--s) = '\0';
+  s += no_digits(n);
+  *s-- = '\0';
   while (n) {
-    *(--s) = digits[n % 10];
+    *s-- = digits[n % 10];
     n /= 10;
   }
 }
@@ -33,14 +33,68 @@ void int_to_string(int n, char *s)
   }
 }
 
+
+void pos_int_to_string_index(int n, char *s)
+{
+  assert(n > 0);
+  static const char *digits = "0123456789";
+  int i = no_digits(n) + 1;
+  s[--i] = '\0';
+  while (n) {
+    s[--i] = digits[n % 10];
+    n /= 10;
+  }
+}
+
+void int_to_string_index(int n, char *s)
+{
+  if (n == 0) {
+    s[0] = '0'; s[1] = '\0';
+  } else if (n < 0) {
+    s[0] = '-';
+    pos_int_to_string_index(-n, s + 1);
+  } else {
+    pos_int_to_string_index(n, s);
+  }
+}
+
+void pos_int_to_string_offset(int n, char *s)
+{
+  assert(n > 0);
+  static const char *digits = "0123456789";
+  int i = no_digits(n) + 1;
+  *(s + --i) = '\0';
+  while (n) {
+    *(s + --i) = digits[n % 10];
+    n /= 10;
+  }
+}
+
+void int_to_string_offset(int n, char *s)
+{
+  if (n == 0) {
+    s[0] = '0'; s[1] = '\0';
+  } else if (n < 0) {
+    s[0] = '-';
+    pos_int_to_string_offset(-n, s + 1);
+  } else {
+    pos_int_to_string_offset(n, s);
+  }
+}
+
+
 int main(int argc, char **argv)
 {
   int n = 11;
   char buf[no_digits(n) + 1 + 1]; // +1 for '-' and +1 for '\0'
+  char buf2[no_digits(n) + 1 + 1]; // +1 for '-' and +1 for '\0'
+  char buf3[no_digits(n) + 1 + 1]; // +1 for '-' and +1 for '\0'
 
   for (int i = -n; i < n; i++) {
     int_to_string(i, buf);
-    printf("%d = '%s'\n", i, buf);
+    int_to_string_index(i, buf2);
+    int_to_string_offset(i, buf3);
+    printf("%d = '%s' / '%s' / '%s'\n", i, buf, buf2, buf3);
   }
 
   return EXIT_SUCCESS;
