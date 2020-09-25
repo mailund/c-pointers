db70eeb201b6955b933954429513e7746d9b01ef
Thomas Mailund <mailund@birc.au.dk>
2020-09-25 04:02:56 +0200

    new head list

--- data-structures/lists/head-singly-linked-list.c
+++ data-structures/lists/head-singly-linked-list.c
@@ -1,5 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
+// fake malloc
+void * (*ml)(size_t) = malloc;
+void *fake_malloc(size_t size)
+{
+  if (rand() % 1000 == 0) {
+    return 0;
+  } else {
+    return ml(size);
+  }
+}
+#define malloc fake_malloc
 
 // for contains
 #include <stdbool.h>
@@ -8,168 +21,234 @@ struct link {
   int value;
   struct link *next;
 };
+typedef struct link list;
 
 struct link *new_link(int val, struct link *next)
 {
-    struct link *link = malloc(sizeof(struct link));
-    link->value = val;
-    link->next = next;
-    return link;
+  struct link *link = malloc(sizeof(struct link));
+  if (!link) return 0;
+  link->value = val;
+  link->next = next;
+  return link;
 }
 
-struct link *make_list(int n, int array[n])
+list *new_list(void)
 {
-    struct link *link = 0;
-    for (int i = n - 1; i >= 0; i--) {
-        link = new_link(array[i], link);
+  // It doesn't matter what val is
+  // we are not supposed to look at it.
+  return new_link(0, 0);
+}
+
+void free_links(struct link *link)
+{
+  while (link) {
+      // Remember next, we cannot get it
+      // after free(list)
+      struct link *next = link->next;
+      free(link);
+      link = next;
+  }
+}
+
+void free_list(list *x)
+{
+  free_links(x->next);
+  x->next = 0;
+}
+
+list *make_list(int n, int array[n])
+{
+  list *x = new_list();
+  if (!x) return 0;
+
+  struct link *links = 0;
+  for (int i = n - 1; i >= 0; i--) {
+    struct link *link = new_link(array[i], links);
+    if (!link) { // Allocation error -- clean up
+      free_list(links);
+      return 0;
     }
-    struct link *head = new_link(0, link);
-    return head;
+    links = link;
+  }
+
+  x->next = links;
+  return x;
 }
 
-void print_list(struct link *list)
+void print_list(list *x)
 {
-    printf("[ ");
-    struct link *link = list->next;
-    while (link) {
-        printf("%d ", link->value);
-        link = link->next;
+  printf("[ ");
+  struct link *link = x->next;
+  while (link) {
+    printf("%d ", link->value);
+    link = link->next;
+  }
+  printf("]\n");
+}
+
+
+bool contains(list *x, int val)
+{
+  struct link *link = x->next;
+  while (link) {
+    if (link->value == val)
+      return true;
+    link = link->next;
+  }
+  return false;
+}
+
+int prepend(list *x, int val)
+{
+  struct link *link = new_link(val, x->next);
+  if (!link) return 0;
+  x->next = link;
+  return 1;
+}
+
+struct link *last_link(list *x)
+{
+  struct link *prev = x;
+  while (prev->next) {
+    prev = prev->next;
+  }
+  return prev;
+}
+
+int append(list *x, int val)
+{
+  struct link *link = new_link(val, 0);
+  if (!link) return 0;
+  struct link *last = last_link(x);
+  last->next = link;
+  return 1;
+}
+
+void concatenate(list *x, list *y)
+{
+  struct link *last = last_link(x);
+  last->next = y->next;
+  y->next = 0;
+}
+
+void delete_value(list *x, int val)
+{
+  struct link *prev = x;
+  struct link *link = x->next;
+  while (link) {
+    if (link->value != val) {
+      prev = link;
+      link = link->next;
+    } else {
+      struct link *next = link->next;
+      free(link);
+      prev->next = link = next;
     }
-    printf("]\n");
+  }
 }
 
-void free_list(struct link *list)
+void reverse(list *x)
 {
-    // Nothing to change here, we also need to free
-    // the head
-    while (list) {
-        // Remember next, we cannot get it
-        // after free(list)
-        struct link *next = list->next;
-        free(list);
-        list = next;
-    }
+  if (!x->next) return;
+
+  struct link *next = x->next->next;
+  struct link *reversed = x->next;
+  reversed->next = 0;
+  while (next) {
+      struct link *next_next = next->next;
+      next->next = reversed;
+      reversed = next;
+      next = next_next;
+  }
+  x->next = reversed;
 }
 
-bool contains(struct link *list, int val)
+int main(void)
 {
-    list = list->next; // pass head
-    while (list) {
-        if (list->value == val)
-            return true;
-        list = list->next;
-    }
-    return false;
-}
-
-void prepend(struct link *list, int val)
-{
-    list->next = new_link(val, list->next);
-}
-
-void append(struct link *list, int val)
-{
-    while (list->next) {
-        list = list->next;
-    }
-}
-
-void concatenate(struct link *x, struct link *y)
-{
-    while (x->next) {
-        x = x->next;
-    }
-    x->next = y->next; // skip head
-    y->next = 0; // make y empty to avoid problems
-}
-
-void append_(struct link *list, int val)
-{
-    // append via concatenate is no longer attractive
-    struct link *y = new_link(0, new_link(val, 0));
-    concatenate(list, y);
-    free(y);
-}
-
-void delete_value(struct link *list, int val)
-{
-    struct link *prev = list;
-    struct link *link = list->next;
-    while (link) {
-        if (link->value != val) {
-            prev = link;
-            link = link->next;
-        } else {
-            struct link *next = link->next;
-            free(link);
-            prev->next = link = next;
-        }
-    }
-}
-
-void reverse(struct link *head)
-{
-    if (!head->next) return;
-    struct link *next = head->next->next;
-    struct link *reversed = head->next; 
-    reversed->next = 0;
-    while (next) {
-        struct link *next_next = next->next;
-        next->next = reversed;
-        reversed = next;
-        next = next_next;
-    }
-    head->next = reversed;
-}
-
-int main(int argc, char **argv)
-{
+  srand(time(0));
   int array[] = { 1, 2, 3, 4, 5 };
-  int n = sizeof(array) / sizeof(int);
-  struct link *list = make_list(n, array);
-  print_list(list);
-  free_list(list);
+  int n = sizeof(array) / sizeof(array[0]);
 
-  list = make_list(n, array);
+  struct link *x = make_list(n, array);
+  if (!x) {
+    perror("List error: ");
+    exit(1); // Just bail here
+  }
+
+  printf("Contains:\n");
   printf("%d %d %d\n",
-          contains(list, 0),
-          contains(list, 3),
-          contains(list, 6));
-  free_list(list);
-  
-  list = make_list(n, array);
-  append(list, 6);
-  prepend(list, 0);
-  print_list(list);
-  free_list(list);
-  
-  list = make_list(n, array);
-  struct link *list2 = make_list(n, array);
-  concatenate(list, list2);
-  print_list(list);
-  free_list(list);
-  free_list(list2); // safe
-  
-  list = make_list(n, array);
-  append_(list, 6);
-  print_list(list);
-  free_list(list);
-  
-  list = make_list(n, array);
-  delete_value(list, 2);
-  delete_value(list, 3);
-  print_list(list);
-  free_list(list);
-  
-  list = make_list(n, array);
-  reverse(list);
-  print_list(list);
-  free_list(list);
+          contains(x, 0),
+          contains(x, 3),
+          contains(x, 6));
+  free_list(x);
+  printf("\n");
 
-  list = new_link(0, 0);
-  reverse(list);
-  print_list(list);
-  free_list(list);
+  printf("prepend/append\n");
+  x = make_list(n, array);
+  if (!x) {
+    perror("List error: ");
+    exit(1); // Just bail here
+  }
 
-  return EXIT_SUCCESS;
+  int success = append(x, 6);
+  if (!success) {
+    perror("List error:");
+    exit(1);
+  }
+  success = prepend(x, 0);
+  if (!success) {
+    perror("List error:");
+    exit(1);
+  }
+  print_list(x);
+  free_list(x);
+  printf("\n");
+
+
+
+
+  printf("concatenate:\n");
+  x = make_list(n, array);
+  if (!x) {
+    perror("List error: ");
+    exit(1);
+  }
+  list *y = make_list(n, array);
+  if (!y) {
+    perror("List error: ");
+    exit(1);
+  }
+
+  concatenate(x, y);
+  print_list(x);
+  free_list(x);
+  printf("\n");
+
+  printf("deleting values:\n");
+  x = make_list(n, array);
+  if (!x) {
+    perror("List error: ");
+    exit(1);
+  }
+  delete_value(x, 2);
+  delete_value(x, 3);
+  print_list(x);
+  free_list(x);
+  printf("\n");
+
+  printf("deleting first link:\n");
+  x = make_list(n, array);
+  delete_value(x, 1);
+  print_list(x);
+  free_list(x);
+  printf("\n");
+
+  printf("reversing:\n");
+  x = make_list(n, array);
+  reverse(x);
+  print_list(x);
+  free_list(x);
+  printf("\n");
+
+  return 0;
 }
