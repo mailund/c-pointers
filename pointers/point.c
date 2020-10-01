2b175d59cc2929a11de1f5fcd1e70e630ccbf142
Thomas Mailund <mailund@birc.au.dk>
2020-10-01 08:23:09 +0200

    updated point example

--- pointers/point.c
+++ pointers/point.c
@@ -1,54 +1,74 @@
 #include <stdio.h>
 
-struct point {
+typedef struct point {
   double x, y;
-};
+} point;
 
-
-void set_point(struct point point, double x, double y)
+point move_point_horisontally(point p, double amount)
 {
-  point.x = x;
-  point.y = y;
+  p.x += amount;
+  return p;
 }
 
-struct point set_point_(struct point point, double x, double y)
+point move_point_vertically(point p, double amount)
 {
-  point.x = x;
-  point.y = y;
-  return point;
+  p.y += amount;
+  return p;
 }
 
-void set_point__(struct point *point, double x, double y)
+point move_point(point p, double delta_x, double delta_y)
 {
-  (*point).x = x;
-  (*point).y = y;
+  p = move_point_horisontally(p, delta_x);
+  p = move_point_vertically(p, delta_y);
+  return p;
 }
 
-void set_point___(struct point *point, double x, double y)
+void print_point(point p)
 {
-  point->x = x;
-  point->y = y;
+  printf("point <%.2f, %.2f>\n", p.x, p.y);
 }
 
-void print_point(struct point *point)
+typedef struct rectangle {
+  point upper_left;
+  point lower_right;
+} rectangle;
+
+rectangle move_rectangle(rectangle rect,
+                         double delta_x,
+                         double delta_y)
 {
-  printf("point <%.2f, %.2f>\n", point->x, point->y);
+  rect.upper_left =
+    move_point(rect.upper_left, delta_x, delta_y);
+  rect.lower_right =
+    move_point(rect.lower_right, delta_x, delta_y);
+  return rect;
+}
+
+void print_rectangle(rectangle rect)
+{
+  printf("rectangle:\n");
+  print_point(rect.upper_left);
+  print_point(rect.lower_right);
+  printf("\n");
 }
 
 int main(void)
 {
-  struct point point = { .x = 0.0, .y = 0.0 };
-  print_point(&point);
+  point p = { .x = 0.0, .y = 0.0 };
+  print_point(p);
 
-  set_point(point, 13, 42);
-  print_point(&point);
+  p = move_point(p, 10, 10);
+  print_point(p);
+  printf("\n");
 
-  point = set_point_(point, 13, 42);
-  print_point(&point);
+  rectangle rect = {
+    .upper_left =  {.x =  0.0, .y = 10.0},
+    .lower_right = {.x = 10.0, .y =  0.0}
+  };
+  print_rectangle(rect);
 
-  set_point___(&point, -13, -42);
-  print_point(&point);
-
+  rect = move_rectangle(rect, 10, 10);
+  print_rectangle(rect);
 
   return 0;
 }
