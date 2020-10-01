acacdd81a9c775f6f34f81e94df6eec7b97bf677
Thomas Mailund <mailund@birc.au.dk>
2020-10-01 07:44:01 +0200

    a little cleanup

--- pointers/strict-alias.c
+++ pointers/strict-alias.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdalign.h>
 
 int f(int *i, long *l)
 {
@@ -14,12 +15,25 @@ int g(char *c, long *l)
   return *c;
 }
 
+int h(double *c, long *l)
+{
+  *c = -1;
+  *l = 0;
+  return (int)*c;
+}
+
 int main(void)
 {
   long x;
+
   int i = f((int *)&x, &x);
-  int j = g((char *)&x, &x);
-  printf("%ld %d %d\n", x, i, j);
+  printf("x == %ld, f(&x,&x) == %d\n", x, i);
+
+  i = g((char *)&x, &x);
+  printf("x == %ld, g(&x,&x) == %d\n", x, i);
+
+  i = h((double *)&x, &x);
+  printf("x == %ld, h(&x,&x) == %d\n", x, i);
 
   return 0;
 }
