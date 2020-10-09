05f9923e4c5cb3490b20fa71303cf537ad877d97
Thomas Mailund <mailund@birc.au.dk>
2020-10-09 07:56:24 +0200

    some cleanup and removing substrings

--- substrings-and-ranges/substrings-ranges.c
+++ substrings-and-ranges/substrings-ranges.c
@@ -9,6 +9,7 @@ struct range {
 };
 
 typedef struct range substr;
+typedef substr substr_iter;
 
 // Use a null begin pointer as a special symbol
 static substr const NULL_SUBSTR = { .begin = 0 };
@@ -18,26 +19,26 @@ int is_null_substr(substr s)
 }
 
 // Creating substr
+#define SUBSTR(b,e) \
+  (substr){ .begin = (b), .end = (e) }
+
 substr as_substr(char *s)
 {
   char *x = s;
   while (*x) x++;
-  return (substr){
-    .begin = s,
-    .end = x
-  };
+  return SUBSTR(s, x);
 }
 
 substr slice(char *s, int i, int j)
 {
   assert(i <= j);
-  return (substr){
-    .begin = s + i,
-    .end   = s + j
-  };
+  return SUBSTR(s + i, s + j);
 }
 
-// Creating a string from a substr
+// Creating a string from a substr.
+// The caller is responsible for ensuring that
+// the to buffer is large enough (i.e. at least
+// substr_len(from) + 1 long).
 char *substr_to_buf(char *to, substr from)
 {
   while (from.begin != from.end) {
@@ -47,8 +48,6 @@ char *substr_to_buf(char *to, substr from)
   return to;
 }
 
-
-
 // IO
 void print_substr(substr s)
 {
@@ -56,6 +55,21 @@ void print_substr(substr s)
     putchar(*s.begin++);
   }
 }
+
+// Mutating substrings to create strings
+char zero_term(substr s)
+{
+  char c = *s.end;
+  *s.end = '\0';
+  return c;
+}
+
+void restore_term(substr s, char c)
+{
+  *s.end = c;
+}
+
+
 
 // Basic operations
 size_t substr_len(substr s)
@@ -80,17 +94,43 @@ int substr_cmp(substr x, substr y)
 
 void substr_rev(substr s)
 {
-  // We need to point at, not past, the last char
-  s.end--;
-  while (s.begin < s.end) {
-    // Swap
-    char x = *s.begin;
-    *s.begin = *s.end;
-    *s.end = x;
-    // increment to next pair
-    s.begin++ ; s.end--;
+  char * restrict x = s.begin;
+  char * restrict y = s.end - 1;
+  for (; x < y; x++, y--) {
+    char c = *x; *x = *y; *y = c;
   }
 }
+
+// The tests for overlaps and such is only valid,
+// according to the langauge standard, if the
+// substrings are ranges of the same buffer. Comparing
+// pointers into separately allocated memory gives no
+// guarantees. But it is the best we can do here...
+
+// Do they overlap with x on the left?
+// This is the only dangerous overlap when it comes to
+// copying from x to y
+int substr_left_overlaps(substr x, substr y)
+{
+  return x.begin < y.begin && x.end > y.begin;
+}
+
+// We write the charcters in from to to, but only
+// part of from if to is shorter than from. The caller
+// is responsible for checking substr_left_verlaps(from, to)
+// if that can be an issue
+substr copy_substr(substr to, substr from)
+{
+  char * restrict x = to.begin;
+  char * restrict y = from.begin;
+  while (x != to.end && y != from.end)
+    *x++ = *y++;
+
+  // The to substring now goes from the character
+  // right after the copy and to the original end
+  return SUBSTR(x, to.end);
+}
+
 
 int empty_substr(substr x)
 {
@@ -113,42 +153,61 @@ int substr_overlaps(substr x, substr y)
   }
 }
 
-// Do they overlap with x on the left?
-// This is the only dangerous overlap when it comes to
-// copying from x to y
-int substr_left_overlaps(substr x, substr y)
-{
-  return x.begin < y.begin && x.end > y.begin;
-}
-
-// We write the charcters in from to to, but only
-// part of from if to is shorter than from. The caller
-// is responsible for checking substr_left_verlaps(from, to)
-// if that can be an issue
-substr copy_substr(substr to, substr from)
-{
-  while (from.begin != from.end &&
-         to.begin != to.end) {
-    *to.begin++ = *from.begin++;
-  }
-  // The to substring now goes from the character
-  // right after the copy and to the original end
-  return to;
-}
-
 // It is the caller's responsibility to check that
 // x and y have the same length and do not overlap
 void swap_substr(substr x, substr y)
 {
-  while (x.begin != x.end && y.begin != y.end) {
-    char c = *x.begin;
-    *x.begin = *y.begin;
-    *y.begin = c;
-    x.begin++; y.begin++;
+  char * restrict p = x.begin;
+  char * restrict q = y.begin;
+  for (; p != x.end && q != y.end; p++, q++) {
+    char c = *p; *p = *q; *q = c;
   }
 }
 
+// Is x a sub-range of y?
+int is_subrange(substr x, substr y)
+{
+  return y.begin <= x.begin && x.end <= y.end;
+}
 
+// Remove substr y from substr x, put the result
+// in to and return the updated substr.
+// It is the caller's responsibility to check that
+// y is a subrange of x.
+substr delete_substr(substr to, substr x, substr y)
+{
+  substr before = SUBSTR(x.begin, y.begin);
+  substr after = SUBSTR(y.end, x.end);
+  substr tmp = copy_substr(to, before);
+  tmp = copy_substr(tmp, after);
+  return SUBSTR(to.begin, tmp.begin);
+}
+
+// With this one, remember that you are modifying
+// x, so other references to it, and subtrings in it
+// will also be affected
+substr delete_substr_inplace(substr x, substr y)
+{
+  substr dest = SUBSTR(y.begin, x.begin);
+  substr after = SUBSTR(y.end, x.end);
+  substr replacement = copy_substr(dest, after);
+  return SUBSTR(x.begin, replacement.begin);
+}
+
+// Find the first occurrence of the string y
+// in the string x. Return a NULL string if
+// there isn't one.
+substr find_occurrence(substr x, substr y)
+{
+  int n = substr_len(y);
+  char *s = x.begin, *end = x.end - n;
+  for (; s < end; s++) {
+    if (strncmp(s, y.begin, n) == 0) {
+      return SUBSTR(s, s + n);
+    }
+  }
+  return NULL_SUBSTR;
+}
 
 // Word iteration
 char *skip_word(char *x)
@@ -165,7 +224,7 @@ char *find_word(char *x)
   return x;
 }
 
-substr next_word(substr *iter)
+substr next_word(substr_iter *iter)
 {
   char *begin = find_word(iter->begin);
   if (*begin == '\0') {
@@ -174,10 +233,7 @@ substr next_word(substr *iter)
   }
   char *end = skip_word(begin);
   iter->begin = end;
-  return (substr){
-    .begin = begin,
-    .end = end
-  };
+  return SUBSTR(begin, end);
 }
 
 // Compacting over iterator...
@@ -188,7 +244,7 @@ substr copy_words(substr to, substr from)
   // sep is used to put spaces between
   // words but not before the first word
   char sep = '\0';
-  substr iter = from;
+  substr_iter iter = from;
   for (substr word = next_word(&iter);
        !is_null_substr(word);
        word = next_word(&iter)) {
@@ -196,9 +252,7 @@ substr copy_words(substr to, substr from)
     sep = ' '; // always space after the first
     to = copy_substr(to, word);
   }
-  return (substr){
-    .begin = begin, .end = to.begin
-  };
+  return SUBSTR(begin, to.begin);
 }
 
 substr compact_words(substr s)
@@ -206,68 +260,52 @@ substr compact_words(substr s)
   return copy_words(s, s);
 }
 
-// Mutating substrings to create strings
-char zero_term(substr s)
-{
-  char c = *s.end;
-  *s.end = '\0';
-  return c;
-}
-
-void restore_term(substr s, char c)
-{
-  *s.end = c;
-}
 
 
 // Iterator for non-overlapping occurrences of s
-substr next_occurrence(substr *iter, substr s)
+substr next_occurrence(substr_iter *iter, substr s)
 {
-  int n = substr_len(s);
-  char *x = iter->begin, *end = iter->end - n;
-  for (; x < end; ++x) {
-    if (strncmp(x, s.begin, n) == 0) {
-      // we found a match
-      iter->begin = x + n; // move to after match
-      return (substr){
-        .begin = x, .end = x + n
-      };
-    }
+  substr occ = find_occurrence(*iter, s);
+  if (!is_null_substr(occ)) {
+    // if there was an occurrence, we must update
+    // the iterator
+    iter->begin = occ.end;
   }
-  // No more matches...
-  return NULL_SUBSTR;
+  return occ;
 }
 
 // FIXME: handle unequal length
 substr replace_substr(substr s, substr from, substr to)
 {
-  substr iter = s;
+  substr_iter iter = s;
   for (substr match = next_occurrence(&iter, from);
        !is_null_substr(match);
        match = next_occurrence(&iter, from)) {
     copy_substr(match, to);
   }
+  return s; // FIXME
 }
 
 int main(void)
 {
-  char s[] = "hello, world";
-  substr ss = as_substr(s);
+  do {
+    char s[] = "hello, world";
+    substr ss = as_substr(s);
+    printf("%zu %zu\n", strlen(s), substr_len(ss));
 
-  printf("%zu %zu\n", strlen(s), substr_len(ss));
+    printf("slicing \""); print_substr(ss); printf("\"");
+    ss = slice(s, 0, 5);
+    printf(" gives us \""); print_substr(ss); printf("\"\n");
+    printf("%zu %zu\n", strlen(s), substr_len(ss));
 
-  printf("slicing \""); print_substr(ss); printf("\"");
-  ss = slice(s, 0, 5);
-  printf(" gives us \""); print_substr(ss); printf("\"\n");
-  printf("%zu %zu\n", strlen(s), substr_len(ss));
+    char buf[substr_len(ss) + 1];
+    substr_to_buf(buf, ss);
+    printf("%s\n", buf);
 
-  char buf[substr_len(ss) + 1];
-  substr_to_buf(buf, ss);
-  printf("%s\n", buf);
-
-  char c = zero_term(ss);
-  printf("%s\n", ss.begin);
-  restore_term(ss, c);
+    char c = zero_term(ss);
+    printf("%s\n", ss.begin);
+    restore_term(ss, c);
+  } while(0);
 
   printf("comparisons\n");
   printf("aa vs a: %d\n", substr_cmp(as_substr("aa"), as_substr("a")));
@@ -302,12 +340,14 @@ int main(void)
 
   // Overlaps
   do {
+    char *s = "foobar";
     // Empty strings do not overlap
     substr x = slice(s, 0, 0);
     substr y = slice(s, 0, 0);
     assert(!substr_overlaps(x, y));
   } while(0);
   do {
+    char *s = "foobar";
     // Empty strings do not overlap
     substr x = slice(s, 0, 1);
     substr y = slice(s, 0, 0);
@@ -316,18 +356,21 @@ int main(void)
   } while(0);
 
   do {
+    char *s = "foobar";
     substr x = slice(s, 0, 1);
     substr y = slice(s, 0, 1);
     assert(substr_overlaps(x, y));
     assert(substr_overlaps(y, x));
   } while(0);
   do {
+    char *s = "foobar";
     substr x = slice(s, 0, 5);
     substr y = slice(s, 0, 5);
     assert(substr_overlaps(x, y));
     assert(substr_overlaps(y, x));
   } while(0);
   do {
+    char *s = "foobar";
     substr x = slice(s, 0, 5);
     substr y = slice(s, 5, 10);
     assert(!substr_overlaps(x, y));
@@ -335,6 +378,7 @@ int main(void)
   } while(0);
 
   do {
+    char *s = "foobar";
     substr x = slice(s, 1, 5);
     substr y = slice(s, 3, 10);
     assert(substr_overlaps(x, y));
@@ -342,10 +386,42 @@ int main(void)
   } while(0);
 
   do {
+    char *s = "foobar";
     substr x = slice(s, 1, 5);
     substr y = slice(s, 3, 10);
     assert(substr_left_overlaps(x, y));
     assert(!substr_left_overlaps(y, x));
+  } while(0);
+
+  do {
+    char *s = "foobar";
+    substr x = slice(s, 1, 5);
+    substr y = slice(s, 1, 5);
+    assert(is_subrange(x, y));
+  } while(0);
+  do {
+    char *s = "foobar";
+    substr x = slice(s, 3, 5);
+    substr y = slice(s, 1, 5);
+    assert(is_subrange(x, y));
+  } while(0);
+  do {
+    char *s = "foobar";
+    substr x = slice(s, 1, 3);
+    substr y = slice(s, 1, 5);
+    assert(is_subrange(x, y));
+  } while(0);
+  do {
+    char *s = "foobar";
+    substr x = slice(s, 0, 5);
+    substr y = slice(s, 1, 5);
+    assert(!is_subrange(x, y));
+  } while(0);
+  do {
+    char *s = "foobar";
+    substr x = slice(s, 1, 6);
+    substr y = slice(s, 1, 5);
+    assert(!is_subrange(x, y));
   } while(0);
 
 
@@ -399,9 +475,26 @@ int main(void)
   } while(0);
   printf("\n");
 
+  printf("removing...\n");
+  do {
+    char x_buf[] = "foobar", z_buf[] = "foobar";
+    substr x = as_substr(x_buf);
+    substr z = as_substr(z_buf);
+    substr y = slice(x_buf, 1, 3);
+
+    print_substr(x); printf(" -> ");
+    substr res = delete_substr(z, x, y);
+    print_substr(res); printf(" # "); print_substr(z); printf("\n");
+    
+    print_substr(x); printf(" -> ");
+    res = delete_substr_inplace(x, y);
+    print_substr(res); printf(" # "); print_substr(x); printf("\n");
+  } while(0);
+  printf("\n");
+
   printf("iterating over words\n");
   char x[] = "\tfoo    bar123baz\nqux321";
-  substr iter = as_substr(x);
+  substr_iter iter = as_substr(x);
   for (substr word = next_word(&iter);
        !is_null_substr(word);
        word = next_word(&iter)) {
