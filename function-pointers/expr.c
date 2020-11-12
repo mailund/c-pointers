dbbee7936c2c4a2945fe03a506aeb90c4ae29c08
Thomas Mailund <mailund@birc.au.dk>
2020-11-12 07:47:12 +0100

    cleanup in expr.c

--- function-pointers/expr.c
+++ function-pointers/expr.c
@@ -5,25 +5,21 @@ typedef void * cls;
 typedef struct { cls *cls; } obj;
 
 #define basetype(x, base) ((base *)x)
-#define vtbl(inst, base) \
-          basetype(((obj *)inst)->cls, base)
+#define vtbl(inst, base)  basetype(((obj *)inst)->cls, base)
 
-void *alloc_cls(size_t cls_size,
-                void (*cls_init)(cls *))
+typedef void (*cls_init)(cls *);
+void *alloc_cls(size_t cls_size, cls_init cls_init)
 {
   cls *cls = malloc(cls_size);
   if (!cls) abort(); // error handling
   cls_init(cls);
   return cls;
 }
-
-#define INIT_CLS(p, init)                    \
-  do {                                       \
-    if (!p)                                  \
-      p = alloc_cls(                         \
-            sizeof *p, (void (*)(cls *))init \
-          );                                 \
-  } while(0)
+#define INIT_CLS(p, init)                     \
+do {                                          \
+  if (!p)                                     \
+    p = alloc_cls(sizeof *p, (cls_init)init); \
+} while(0)
 
 void *alloc_obj(size_t obj_size, cls cls)
 {
@@ -45,14 +41,9 @@ typedef struct {
   double (*eval) (EXP);
 } base_expr_cls;
 
-void print_expr(EXP e)
-{
-  vtbl(e, base_expr_cls)->print(e);
-}
-double eval_expr(EXP e)
-{
-  return vtbl(e, base_expr_cls)->eval(e);
-}
+// dynamic dispatch
+void   print(EXP e) { vtbl(e, base_expr_cls)->print(e); }
+double eval (EXP e) { return vtbl(e, base_expr_cls)->eval(e); }
 
 void init_base_expr_cls(base_expr_cls *cls)
 {
@@ -61,19 +52,14 @@ void init_base_expr_cls(base_expr_cls *cls)
 }
 
 // Base class, object definition
-typedef struct base_expr {
-  obj obj;
-} base_expr;
+typedef struct { obj obj; } base_expr;
 
-// Base class, methods
-void init_base_expr(base_expr *inst)
-{
-  // nothing to initialise...
-}
+// Base class, methods (init does nothing)
+void init_base_expr(base_expr *inst) {}
 
 
 // Value expressions
-typedef struct  {
+typedef struct {
   base_expr_cls base_expr_cls;
 } value_expr_cls;
 typedef struct {
@@ -97,10 +83,10 @@ double value_expr_eval(value_expr *val)
 void init_value_expr_cls(value_expr_cls *cls)
 {
   init_base_expr_cls(basetype(cls, base_expr_cls));
-  basetype(cls, base_expr_cls)->print =
-            (void (*)(EXP))value_expr_print;
-  basetype(cls, base_expr_cls)->eval  =
-            (double (*)(EXP))value_expr_eval;
+  // override virtual functions
+  base_expr_cls *base_expr = basetype(cls, base_expr_cls);
+  base_expr->print = (void (*)(EXP))value_expr_print;
+  base_expr->eval  = (double (*)(EXP))value_expr_eval;
 }
 
 void init_value_expr(value_expr *val, double value)
@@ -121,33 +107,28 @@ EXP value(double value)
 // Binary expressions
 typedef struct {
   base_expr_cls base_expr_cls;
-} binary_expr_cls;
+} binexpr_cls;
 typedef struct {
   base_expr base_expr;
-  char symb;
-  EXP left, right;
-} binary_expr;
+  char symb; EXP left, right;
+} binexpr;
 
-void print_binary_expr(binary_expr *binop)
+void print_binexpr(binexpr *binop)
 {
-  putchar('(');
-    print_expr(binop->left);
-  putchar(')');
+  putchar('('); print(binop->left); putchar(')');
   putchar(binop->symb);
-  putchar('(');
-    print_expr(binop->right);
-  putchar(')');
+  putchar('('); print(binop->right); putchar(')');
 }
 
-void init_binary_expr_cls(binary_expr_cls *cls)
+void init_binexpr_cls(binexpr_cls *cls)
 {
   init_base_expr_cls(basetype(cls, base_expr_cls));
-  basetype(cls, base_expr_cls)->print =
-          (void (*)(EXP))print_binary_expr;
+  base_expr_cls *base_expr = basetype(cls, base_expr_cls);
+  base_expr->print = (void (*)(EXP))print_binexpr;
 }
 
-void init_binary_expr(binary_expr *binop,
-                      char symb, EXP left, EXP right)
+void init_binexpr(binexpr *binop, char symb,
+                  EXP left, EXP right)
 {
   init_base_expr(basetype(binop, base_expr));
   binop->symb = symb;
@@ -156,34 +137,28 @@ void init_binary_expr(binary_expr *binop,
 }
 
 // Addition
-typedef struct {
-  binary_expr_cls binary_expr_cls;
-} add_expr_cls;
-typedef struct {
-  binary_expr binary_expr;
-} add_expr;
+typedef struct { binexpr_cls binexpr_cls; } add_expr_cls;
+typedef struct { binexpr binexpr;         } add_expr;
 
 // Concrete class, so must have a struct
 add_expr_cls *ADD_EXPR_CLS = 0; // must be initialised
 
-
 double eval_add_expr(add_expr *expr)
 {
-  return eval_expr(basetype(expr, binary_expr)->left) +
-         eval_expr(basetype(expr, binary_expr)->right);
+  binexpr *base = basetype(expr, binexpr);
+  return eval(base->left) + eval(base->right);
 }
 
 void init_add_expr_cls(add_expr_cls *cls)
 {
-  init_binary_expr_cls(basetype(cls, binary_expr_cls));
-  basetype(cls, base_expr_cls)->eval =
-          (double (*)(EXP))eval_add_expr;
+  init_binexpr_cls(basetype(cls, binexpr_cls));
+  base_expr_cls *base_expr = basetype(cls, base_expr_cls);
+  base_expr->eval = (double (*)(EXP))eval_add_expr;
 }
 
 void init_add_expr(add_expr *expr, EXP left, EXP right)
 {
-  init_binary_expr(basetype(expr, binary_expr),
-                   '+', left, right);
+  init_binexpr(basetype(expr, binexpr), '+', left, right);
 }
 
 // constructor
@@ -195,36 +170,29 @@ EXP add(EXP left, EXP right)
   return (EXP)expr;
 }
 
-
 // Subtraction
-typedef struct {
-  binary_expr_cls binary_expr_cls;
-} sub_expr_cls;
-typedef struct {
-  binary_expr binary_expr;
-} sub_expr;
+typedef struct { binexpr_cls binexpr_cls; } sub_expr_cls;
+typedef struct { binexpr binexpr;         } sub_expr;
 
 // Concrete class, so must have a struct
 sub_expr_cls *SUB_EXPR_CLS = 0; // must be initialised
 
-
 double eval_sub_expr(sub_expr *expr)
 {
-  return eval_expr(basetype(expr, binary_expr)->left) -
-         eval_expr(basetype(expr, binary_expr)->right);
+  binexpr *base = basetype(expr, binexpr);
+  return eval(base->left) - eval(base->right);
 }
 
 void init_sub_expr_cls(sub_expr_cls *cls)
 {
-  init_binary_expr_cls(basetype(cls, binary_expr_cls));
-  basetype(cls, base_expr_cls)->eval =
-          (double (*)(EXP))eval_sub_expr;
+  init_binexpr_cls(basetype(cls, binexpr_cls));
+  base_expr_cls *base_expr = basetype(cls, base_expr_cls);
+  base_expr->eval = (double (*)(EXP))eval_sub_expr;
 }
 
 void init_sub_expr(add_expr *expr, EXP left, EXP right)
 {
-  init_binary_expr(basetype(expr, binary_expr),
-                   '-', left, right);
+  init_binexpr(basetype(expr, binexpr), '-', left, right);
 }
 
 // constructor
@@ -243,8 +211,8 @@ int main(void)
         sub(value(10.0),
             value(2.0))
     );
-  print_expr(expr); putchar('\n');
-  printf("evaluates to %f\n", eval_expr(expr));
+  print(expr); putchar('\n');
+  printf("evaluates to %f\n", eval(expr));
 
   return 0;
 }
