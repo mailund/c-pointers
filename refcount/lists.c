352f95176652ec448e33a0b60f820f833d735976
Thomas Mailund <mailund@birc.au.dk>
2020-11-23 09:08:21 +0100

    slightly better code

--- refcount/lists.c
+++ refcount/lists.c
@@ -37,17 +37,23 @@ void free_link(struct link *link)
 }
 
 // Empty lists and errors
+#define is_error(x) ((x) == 0)
 struct link NIL_LINK = { .refcount = 1 };
 #define is_nil(x)   ((x) == &NIL_LINK)
-#define is_error(x) ((x) == 0)
+#define NIL incref(&NIL_LINK)
 
+// Give a reference; you lose it yourself
 #define takes
+#define give(x) x
+
+#if 0
+// GCC compiler extension statement-expression for give()
+#define give(x)  ({ void *p = x; x = 0; p; })
+#endif
+
+// Let the function use your reference
 #define borrows
 
-#define give(x) x
-#define share(x) incref(x)
-
-#define NIL incref(&NIL_LINK)
 
 list cons(int head, takes list tail)
 {
@@ -56,10 +62,11 @@ list cons(int head, takes list tail)
   list new_link = malloc(sizeof *new_link);
   if (!new_link) { decref(tail); return 0; }
 
+#warning debug info
   links++;
   struct link link_data = {
     .refcount = 1,
-    .next  = tail, // gives away the reference
+    .next  = give(tail), // gives away the reference
     .value = head
   };
   memcpy(new_link, &link_data, sizeof *new_link);
@@ -99,7 +106,7 @@ int length_rec(takes list x, int acc)
   } else {
     struct link *next = incref(x->next);
     decref(x);
-    return length_rec(next, acc + 1);
+    return length_rec(give(next), acc + 1);
   }
 }
 #define length(x) length_rec(x, 0)
@@ -132,16 +139,51 @@ list reverse_rec(takes list x, takes list acc)
   }
   if (is_nil(x)) {
     decref(x);
-    return acc;
+    return give(acc);
+
   } else {
     int value = x->value;
     struct link *next = incref(x->next);
     decref(x);
-    return reverse_rec(next, cons(value, acc));
+    return reverse_rec(give(next), cons(value, acc));
   }
 }
 
 #define reverse(x) reverse_rec(x, NIL)
+
+#endif
+
+// CONCAT --------------------------------------
+#if 0
+list concat(borrows list x, borrows list y)
+{
+  if (is_error(x) || is_error(y)) {
+    return 0;
+  }
+  if (is_nil(x)) {
+    return incref(y); // we return a new ref, so we must incref here
+  } else {
+    return cons(x->value, concat(x->next, y));
+  }
+}
+
+#else
+
+list concat(takes list x, takes list y)
+{
+  if (is_error(x) || is_error(y)) {
+    decref(x); decref(y);
+    return 0;
+  }
+  if (is_nil(x)) {
+    return give(y);
+  } else {
+    int value = x->value;
+    struct link *next = incref(x->next);
+    decref(x);
+    return cons(value, concat(give(next), give(y)));
+  }
+}
 
 
 #endif
@@ -153,19 +195,22 @@ int main(void)
   list x = cons(1, cons(2, cons(3, NIL)));
   print_list(x);
 
-  list y = cons(0, share(x));
+  list y = cons(0, incref(x));
   print_list(y);
 
   decref(x);
   print_list(y);
-  decref(y);
+
+  list z = cons(-1, give(y)); // consider y gone!
+  print_list(z);
+  decref(z);
 
   printf("\n%d links left\n\n", links);
 
   printf("LENGTH\n");
   x = cons(1, cons(2, cons(3, NIL)));
   print_list(x);
-  printf("len(x) == %d\n", length(share(x)));
+  printf("len(x) == %d\n", length(incref(x)));
   print_list(x);
   // leaks if length doesn't free its input
   printf("len(x) == %d\n", length(cons(1, cons(2, cons(3, NIL)))));
@@ -179,13 +224,29 @@ int main(void)
   printf("REVERSE\n");
 
   x = cons(1, cons(2, cons(3, NIL)));
-  y = reverse(x);
+  print_list(x);
+  y = reverse(give(x));
   print_list(y);
-  decref(x);
   decref(y);
 
   // would leak if reverse and length didn't free their input
   printf("%d\n", length(reverse(cons(1, cons(2, cons(3, NIL))))));
+
+  printf("\n%d links left\n\n", links);
+
+  printf("CONCAT\n");
+  x = cons(1, cons(2, cons(3, NIL)));
+  y = cons(4, cons(5, NIL));
+  z = concat(incref(x), incref(y));
+
+  print_list(z);
+  decref(x); decref(y);
+  print_list(z);
+  decref(z);
+
+  z = concat(cons(1, cons(2, NIL)), cons(11, cons(12, cons(13, NIL))));
+  print_list(z);
+  decref(z);
 
   printf("\n%d links left\n\n", links);
 
